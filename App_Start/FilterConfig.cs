da97f437585bcbaecc669ceda11c5985a0da4e26
This PC <This PC@DESKTOP-TK56C1K>
2024-06-06 16:43:42 +0530

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MundriSoftEmployeeDetails
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
